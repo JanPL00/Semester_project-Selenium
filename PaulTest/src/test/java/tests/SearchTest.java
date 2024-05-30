fa0fef47888d8c97aaa0ffd38c65fd06ce4700c0
Jan Plaček <placeja5@fel.cvut.cz>
2024-05-31 01:48:22 +0200

    finished the first test 🥖

--- PaulTest/src/test/java/tests/SearchTest.java
+++ PaulTest/src/test/java/tests/SearchTest.java
@@ -4,7 +4,15 @@ import POM.PaulHomePage;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import setup.InitClass;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static setup.InitClass.driver;
 
 public class SearchTest {
 
@@ -22,8 +30,15 @@ public class SearchTest {
     @Test
     public void testSearch() throws InterruptedException {
 
-        PaulHomePage paulHomePage = new PaulHomePage(InitClass.driver);
+        PaulHomePage paulHomePage = new PaulHomePage(driver);
         paulHomePage.searchProduct("bageta");
+
+        //Validate the search results
+        WebElement searchResults = driver.findElement(By.xpath("/html/body/div[3]/div/main/ul"));
+        List<WebElement> resultItems = searchResults.findElements(By.tagName("li"));
+
+        // Assert that there are results
+        assertFalse(resultItems.isEmpty(), "The search results should not be empty.");
 
     }
 }
